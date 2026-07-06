40d5fe3aaa88cd54c5386c9f042b49eccf73cf03
phamhuyanhtu <anhtuphamhuy1234@gmail.com>
2026-07-06 21:05:25 +0700

    Learn Js

---
+++ js/main.js
@@ -0,0 +1,47 @@
+document.querySelector("button").onclick 
+=function(){
+    alert("Bạn vừa bấm đăng nhập")
+}
+console.log(1+1)
+console.log("2")
+console.error("Lỗi ")
+console.info("Thông tin")
+console.warn("Cảnh báo")
+let students = [
+    "Tú",
+    "Khang",
+    "Thịnh",
+    "Nhi" ,
+    "Tùng"
+ ] ;
+console.table(students) ;
+// biến trong js
+// let : tên biến , age : tên biến , 25 : giá trị
+let age = 18 ;
+console.log(age)
+// Nối chuỗi
+console.log("age : "+age)
+// thay đổi giá trị
+age = 36
+console.log("Age sau khi thay đổi : "+age)
+// const = api_url
+const Pi = 3.14 ; //const là hằng số , ko đc gán lại gtri khác  
+// Quy tắc đặt tên biến :
+// Bắt đầu bằng chữ , _ , $
+// Ko chứa kí tự đặc biệt
+// Ko bắt đầu bằng số , ko có khoảng trắng
+// Ko phải là từ khóa (VD : let , var , console , math ...)
+// Quy ước :
+// Đặt tên biến có ý nghĩa , ngắn gọn
+// Biến ko đặt trùng tên
+
+// Cho nhập vào tên và log ra tên người đó
+let ten = prompt()
+console.log("Xin chào : " + ten)
+let nghe = prompt("Bạn làm nghề gì : ")
+console.log("Xin chào : " + nghe)
+// Yêu cầu nhập cạnh hình vuông , tính diện tích
+let canh = prompt("Cạnh : ")
+console.log("Diện tích hình vuông : " +(canh*canh))
+// + , - , * , /
+
